ff350c06d9e128c5b73ad5882ea9e8ed2464d5be
fed67 <you@example.com>
2025-03-16 12:00:59 +0100

    Update buildroot configs

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 14003ae06329c3f71b73c0a29d1782bc46affed8
+AESD_ASSIGNMENTS_VERSION = dd42dd08a83b2c693bd04cdaa3a68bbebaf102d6
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,12 +16,13 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 # AESD_ASSIGNMENTS_PATCH = testae.patch
 
+AESD_ASSIGNMENTS_MODULE_SUBDIRS = aesd-char-driver/
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
-
+# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/aesd-char-driver
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
@@ -35,7 +36,12 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+
+
 endef
 # $(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
